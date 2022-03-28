5643b59238a4e13bd9098a47eacc57c0b5816574
Sanektys <Sanektisru@gmail.com>
2022-03-29 01:59:59 +0700

    Added comments to the class "GameObject"

--- Battle City/GameObject.h
+++ Battle City/GameObject.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Level.h"
+#include "level.h"
 
 
 class Game;
@@ -9,94 +9,275 @@ class Game;
 /// <summary>
 /// Базовый класс для всех игровых объектов
 /// <para>
-/// Рассчётные x,y координаты объектов расположены в левом верхнем углу
+/// Расчётные x,y координаты объектов расположены в левом верхнем углу
 /// относительно их габаритов
 /// </para>
 /// </summary>
 class GameObject
 {
     public :
-	    GameObject();
-		virtual ~GameObject() {}
+        GameObject();
+        virtual ~GameObject() {}
 
-		virtual void render(sf::RenderWindow* rw);
-		virtual void update(float dt);
+        /// <summary>
+        /// Базовый метод отрисовки игровых объектов
+        /// <para>Позиционирует спрайт объекта на игровой плоскости</para>
+        /// </summary>
+        /// <param name="rw">- указатель на объект окна игры</param>
+        virtual void render(sf::RenderWindow* rw);
+        /// <summary>
+        /// Основной метод обновления состояния игровых объектов
+        /// <para>Проверяется застревание одного объекта в другом,
+        /// а также вычисляется текущая позиция объекта на основе
+        /// его скорости и прошедшего времени за предыдущий такт</para>
+        /// </summary>
+        /// <param name="dt">- время, прошедшее за предыдущий такт
+        /// (время обработки логики и отрисовки за прошлый раз)</param>
+        virtual void update(float dt);
 
-		virtual void intersect(class GameObject* object);
-		void escapeSticking();
+        /// <summary>
+        /// Метод поведения игрового объекта при столкновении
+        /// с другим объектом
+        /// </summary>
+        /// <param name="object">- объект, с которым столкнулся
+        /// исходный объект, вызвавший метод</param>
+        virtual void intersect(class GameObject* object) {}
+        /// <summary>
+        /// Метод избежания застревания игрового объекта в другом объекте
+        /// </summary>
+        void escapeSticking();
 
-		void doDamage(int damage);
+        /// <summary>
+        /// Метод принятия урона игровым объектом
+        /// </summary>
+        /// <param name="damage">- урон, который получил игровой объект</param>
+        void doDamage(int damage);
 
-		// Блок сеттеров/геттеров
+        // Блок сеттеров/геттеров
+        /////////////////////////////////////////       
+        /// <summary>
+        /// Метод извлекает требуемую текстуру из атласа
+        /// </summary>
+        /// <param name="rect">- прямоугольник "вырезающий"
+        /// нужный спрайт из атласа</param>
+        virtual void setTextureRect(sf::IntRect rect) {}
+        
+        /// <summary>
+        /// Метод возвращает тип объекта из его экземпляра
+        /// </summary>
+        /// <returns>Возвращает тип игрового объекта</returns>
+        enum class GameObjectType getType() const { return _type; }
 
-		virtual void setTextureRect(sf::IntRect rect);
-		
-		enum class GameObjectType getType() { return _type; }
+        /// <summary>
+        /// Связывает игровой объект с общим объектом игровой логики,
+        /// путём сохранения указателя на последний, с целью дальнейшего
+        /// использования его методов
+        /// </summary>
+        /// <param name="game">- указатель на общий для всех
+        /// объект игровой логики</param>
+        void setGame(class Game* game) { _game = game; }
 
-		void setGame(class Game* game) { _game = game; }
+        /// <summary>
+        /// Установка положения игрового объекта по горизонтали
+        /// </summary>
+        /// <param name="x">- координата по горизонтали</param>
+        void setX(float x) { _x = x; }
+        /// <summary>
+        /// Получение положения игрового объекта по горизонтали
+        /// </summary>
+        /// <returns>Возвращает координату по горизонтали x объекта</returns>
+        float getX() const { return _x; }
 
-		void setX(float x) { _x = x; }
-		float getX() { return _x; }
+        /// <summary>
+        /// Установка положения игрового объекта по вертикали
+        /// </summary>
+        /// <param name="y">- координата по вертикали</param>
+        void setY(float y) { _y = y; }
+        /// <summary>
+        /// Получение положения игрового объекта по вертикали
+        /// </summary>
+        /// <returns>Возвращает координату по вертикали y объекта</returns>
+        float getY() const { return _y; }
 
-		void setY(float y) { _y = y; }
-		float getY() { return _y; }
+        /// <summary>
+        /// Установка горизонтальной скорости игрового объекта
+        /// <para>Положительная скорость - движение вправо,
+        /// а отрицательная - влево</para>
+        /// </summary>
+        /// <param name="xSpeed">- горизонтальная скорость</param>
+        void setXSpeed(float xSpeed) { _xSpeed = xSpeed; }
+        /// <summary>
+        /// Получение горизонтальной скорости игрового объекта
+        /// </summary>
+        /// <returns>Возвращает горизонтальную скорость объекта</returns>
+        float getXSpeed() const { return _xSpeed; }
 
-		void setXSpeed(float xSpeed) { _xSpeed = xSpeed; }
-		float getXSpeed() { return _xSpeed; }
+        /// <summary>
+        /// Установка вертикальной скорости игрового объекта
+        /// <para>Положительная скорость - движение вниз,
+        /// а отрицательная - вверх</para>
+        /// </summary>
+        /// <param name="ySpeed">- вертикальная скорость объекта</param>
+        void setYSpeed(float ySpeed) { _ySpeed = ySpeed; }
+        /// <summary>
+        /// Получение вертикальной скорости игрового объекта
+        /// </summary>
+        /// <returns>Возвращает вертикальную скорость объекта</returns>
+        float getYSpeed() const { return _ySpeed; }
 
-		void setYSpeed(float ySpeed) { _ySpeed = ySpeed; }
-		float getYSpeed() { return _ySpeed; }
+        /// <summary>
+        /// Установка ширины игрового объекта
+        /// </summary>
+        /// <param name="width">- ширина объекта</param>
+        void setWidth(float width) { _width = width; }
+        /// <summary>
+        /// Получение ширины игрового объекта
+        /// </summary>
+        /// <returns>Возвращает ширину объекта</returns>
+        float getWidth() const { return _width; }
 
-		void setWidth(float width) { _width = width; }
-		float getWidth() { return _width; }
+        /// <summary>
+        /// Установка высоты игрового объекта
+        /// </summary>
+        /// <param name="height">- высота объекта</param>
+        void setHeight(float height) { _height = height; }
+        /// <summary>
+        /// Получение высоты игрового объекта
+        /// </summary>
+        /// <returns>Возвращает высоту объекта</returns>
+        float getHeight() const { return _height; }
 
-		void setHeight(float height) { _height = height; }
-		float getHeight() { return _height; }
+        /// <summary>
+        /// Установка очков прочности(здоровья) игрового объекта
+        /// </summary>
+        /// <param name="health">- очки прочности объекта</param>
+        void setHealth(int health) { _health = health; }
+        /// <summary>
+        /// Получение очков прочности(здоровья) игрового объекта
+        /// </summary>
+        /// <returns>Возвращает очки прочности</returns>
+        int getHealth() const { return _health; }
 
-		void setHealth(int health) { _health = health; }
-		int getHealth() { return _health; }
+        /// <summary>
+        /// Установка права удаления экземпляра игрового объекта
+        /// после падения его очков прочности до нуля
+        /// </summary>
+        /// <param name="destroyAfterDeath">- удалять ли объект
+        /// после уничтожения</param>
+        void setDestroyAfterDeath(bool destroyAfterDeath)
+            { _destroyAfterDeath = destroyAfterDeath; }
+        /// <summary>
+        /// Получение условной переменной, определяющей удаляется ли экземпляр
+        /// игрового объекта при падении его очков прочности до нуля
+        /// </summary>
+        /// <returns>Возвращает true если объект удаляется при уничтожении и
+        /// false если экземпляр остаётся в массиве объектов</returns>
+        bool getDestroyAfterDeath() const { return _destroyAfterDeath; }
 
-		void setDestroyAfterDeath(bool destroyAfterDeath) { _destroyAfterDeath = destroyAfterDeath; }
-		bool getDestroyAfterDeath() { return _destroyAfterDeath; }
+        /// <summary>
+        /// Установка состояния неуязвимости игрового объекта к урону
+        /// </summary>
+        /// <param name="invulnerable">- является ли объект неуязвимым</param>
+        void setInvulnerable(bool invulnerable) { _invulnerable = invulnerable; }
+        /// <summary>
+        /// Получение условной переменной, определяющей
+        /// является ли игровой объект неуязвимым
+        /// </summary>
+        /// <returns>Возвращает true если объект неуязвим,
+        /// false если уязвим к урону</returns>
+        bool getInvulnerable() const { return _invulnerable; }
 
-		void setInvulnerable(bool invulnerable) { _invulnerable = invulnerable; }
-		bool getInvulnerable() { return _invulnerable; }
+        /// <summary>
+        /// Установка состояния невосприимчивости игрового объекта к коллизии
+        /// </summary>
+        /// <param name="physical">- есть ли у объекта коллизия</param>
+        void setPhysical(bool physical) { _physical = physical; }
+        /// <summary>
+        /// Получение условной переменной, определяющей
+        /// является ли игровой объект невосприимчивым к коллизии
+        /// </summary>
+        /// <returns>Возвращает true если объект имеет коллизию,
+        /// false если объект не имеет коллизии</returns>
+        bool getPhysical() const { return _physical; }
 
-		void setPhysical(bool physical) { _physical = physical; }
-		bool getPhysical() { return _physical; }
+        /// <summary>
+        /// Установка направления движения игрового объекта
+        /// </summary>
+        /// <param name="direction">- направление ориентации/движения объекта</param>
+        void setDirection(enum class Direction direction) { _direction = direction; }
+        /// <summary>
+        /// Получение направления движения игрового объекта
+        /// </summary>
+        /// <returns>Возвращает текущее направление движения/ориентации объекта</returns>
+        enum class Direction getDirection() const { return _direction; }
 
-		void setDirection(enum class Direction direction) { _direction = direction; }
-		enum class Direction getDirection() { return _direction; }
+        /// <summary>
+        /// Получение условной переменной,
+        /// определяющей застрял ли игровой объект в другом объекте
+        /// </summary>
+        /// <returns>Возвращает true если объект застрял, false если нет</returns>
+        bool getIsSticking() const { return _isSticking; }
 
-		bool getIsSticking() { return _isSticking; }
-
-		void setInBypass(bool inBypass) { _inBypass = inBypass; }
-		bool getInBypass() { return _inBypass; }
+        /// <summary>
+        /// Установка состояния игрового объекта,
+        /// когда он намеренно проходит сквозь другой объект
+        /// </summary>
+        /// <param name="inBypass">- проходит ли объект
+        /// намеренно сквозь другой объект</param>
+        void setInBypass(bool inBypass) { _inBypass = inBypass; }
+        /// <summary>
+        /// Получение условной переменной, определяющей
+        /// проходит ли игровой объект намеренно сквозь другой объект
+        /// </summary>
+        /// <returns>Возвращает true если объект проходит сквозь другой объект,
+        /// false если нет</returns>
+        bool getInBypass() const { return _inBypass; }
 
     protected :
-		class Game* _game;
-		enum class GameObjectType _type;
+        // Указатель на класс игровой логики для вызова его методов
+        class Game* _game{nullptr};
+        // Тип игрового объекта
+        enum class GameObjectType _type;
 
-		sf::Sprite* _spriteTerrain;
-		sf::Sprite* _spriteEntity;
+        // Каждый объект использует только один из двух представленных спрайтов
+        // в зависимости от своего типа
+        ////////////////
+        sf::Sprite* _spriteTerrain{nullptr}; // Указатель на спрайт окружения
+        sf::Sprite* _spriteEntity{nullptr}; // Указатель на спрайт сущности
 
     private :
-		float _x;
-		float _y;
-		float _xSpeed;
-		float _ySpeed;
+        GameObject(const GameObject&) = delete;
+        GameObject operator=(const GameObject&) = delete;
 
-		float _width;
-		float _height;
+    private :
+        // Координаты игрового объекта
+        ////////////////
+        float _x{0.0f}; // Позиция по горизонтали
+        float _y{0.0f}; // Позиция по вертикали
+        // Скорость игрового объекта по осям
+        ////////////////
+        float _xSpeed{0.0f}; // Скорость по горизонтали
+        float _ySpeed{0.0f}; // Скорость по вертикали
 
-		int _health;
-		bool _destroyAfterDeath;
-		bool _invulnerable;
+        // Габариты игрового объекта
+        ////////////////
+        float _width{1.0f};  // Ширина объекта
+        float _height{1.0f}; // Высота объекта
 
-		bool _physical;
+        // Текущее направление игрового объекта
+        enum class Direction _direction;
 
-		enum class Direction _direction;
+        // Очки прочности объекта или неуязвимость
+        ////////////////
+        int _health{1}; // Здоровье (очки прочности) игрового объекта
+        bool _destroyAfterDeath{true}; // Удалять или нет объект после уничтожения
+        bool _invulnerable{false}; // Является ли объект неуязвимым
 
-		bool _isSticking;
-		bool _inBypass;
+        // Подвержен ли объект коллизии с другими объектами
+        bool _physical{true};
+
+        // Состояния объекта при застревании в других объектах
+        ////////////////
+        bool _isSticking{false}; // Застрял или нет объект в другом объекте
+        bool _inBypass{false}; // Проходит ли намеренно объект сквозь другой объект
 };
--- Battle City/gameObject.cpp
+++ Battle City/gameObject.cpp
@@ -1,244 +1,219 @@
 #include "GameObject.h"
-#include "Level.h"
 #include "Game.h"
 #include "CoordPoint.h"
 #include "Utils.h"
 
 
 GameObject::GameObject() {
-	_game = 0;
 	_type = GameObjectType::NONE;
 
-	_x = 0.0;
-	_y = 0.0;
-	_xSpeed = 0.0;
-	_ySpeed = 0.0;
-
-	_width = 1;
-	_height = 1;
-
-	_health = 1;
-	_destroyAfterDeath = true;
-	_invulnerable = false;
-
-	_physical = true;
-
 	_direction = Direction::UP;
-
-	_spriteTerrain = nullptr;
-	_spriteEntity  = nullptr;
-
-	_isSticking = false;
-	_inBypass = false;
-}
-
-void GameObject::setTextureRect(sf::IntRect rect) {
-	
 }
 
 void GameObject::render(sf::RenderWindow* rw) {
+    // Если объекту присвоен свой спрайт
 	if (_spriteTerrain || _spriteEntity) {
-		float row = _y;
-		float column = _x;
-
 		if (_spriteTerrain) {
-			_spriteTerrain->setPosition(column * PIXELS_PER_CELL + (getWidth() * PIXELS_PER_CELL) / 2.0f,
-				                        row * PIXELS_PER_CELL + (getHeight() * PIXELS_PER_CELL) / 2.0f);
+            // Помещение спрайта на игровом поле.
+            // Предполагается, что центр спрайта смещён на центр самой текстуры
+			_spriteTerrain->setPosition(getX() * PIXELS_PER_CELL
+                                        + (getWidth() * PIXELS_PER_CELL) / 2.0f,
+				                        getY() * PIXELS_PER_CELL
+                                        + (getHeight() * PIXELS_PER_CELL) / 2.0f);
 			rw->draw(*_spriteTerrain);
 		}
 		else if (_spriteEntity) {
-			_spriteEntity->setPosition(column * PIXELS_PER_CELL + (getWidth() * PIXELS_PER_CELL) / 2.0f,
-				                       row * PIXELS_PER_CELL + (getHeight() * PIXELS_PER_CELL) / 2.0f);
+			_spriteEntity->setPosition(getX() * PIXELS_PER_CELL
+                                       + (getWidth() * PIXELS_PER_CELL) / 2.0f,
+				                       getY() * PIXELS_PER_CELL
+                                       + (getHeight() * PIXELS_PER_CELL) / 2.0f);
 			rw->draw(*_spriteEntity);
 		}
 	}
 }
 
 void GameObject::update(float dt) {
-	switch (_type) {
+	switch (getType()) {
 	    case GameObjectType::TANK_ENEMY :
 	    case GameObjectType::TANK_FIRST_PLAYER :
 		case GameObjectType::TANK_SECOND_PLAYER : {
-			if (!_inBypass)
-			    _isSticking = _game->checkIntersects(_x, _y, _width, _height, this);
-
-			if (_isSticking)
+            // Если в данный момент объект не проходит намеренно сквозь другой объект
+			if (!getInBypass())
+                // То произвести проверку на коллизию
+			    _isSticking = _game->checkIntersects(getX(), getY(),
+                                                     getWidth(), getHeight(), this);
+            // Если объект застрял в другом объекте
+			if (getIsSticking())
 				escapeSticking();
 		}
 		
-		case GameObjectType::BULLET : {
-			float oldRow = _y;
-			float oldColumn = _x;
+        case GameObjectType::BULLET : {
+            float oldX = getX();
+            float oldY = getY();        
 
-			float newY = _y + _ySpeed * dt;
-			float newX = _x + _xSpeed * dt;
-
-			float newRow = newY;
-			float newColumn = newX;
-
-			if ((oldColumn != newColumn) && !_isSticking && !_inBypass)
-				_game->moveObjectTo(this, newX, _y);
-			else 
-				_x = newX;
-
-			if ((oldRow != newRow) && !_isSticking && !_inBypass)
-				_game->moveObjectTo(this, _x, newY);
-			else
-				_y = newY;
-
-			break;
-		}
+            float newX = getX() + getXSpeed() * dt; // Новая позиция по горизонтали
+            float newY = getY() + getYSpeed() * dt; // Новая позиция по вертикали
+            // Новые координаты высчитываются по текущей скорости и дельте времени,
+            // затраченной на предыдущий такт обновления игровой логики
+         
+            // Если одна из двух координат не совпадает с предыдущей,
+            // то объект перемещается
+            if ((oldX != newX || oldY != newY) && !_isSticking && !_inBypass) {
+                _game->moveObjectTo(this, newX, newY);
+            } else {
+                setX(newX);
+                setY(newY);
+            }
+        }
+        break;
 	}
 }
 
-void GameObject::intersect(class GameObject* object) {
-
-}
-
-//
 void GameObject::escapeSticking() {
-		//
-		CoordPoint topLeftPoint;
-		CoordPoint topRightPoint;
-		CoordPoint bottomRightPoint;
-		CoordPoint bottomLeftPoint;
+    CoordPoint topLeftPoint;
+    CoordPoint topRightPoint;
+    CoordPoint bottomRightPoint;
+    CoordPoint bottomLeftPoint;
 		
-		topLeftPoint.setCoordPoint(this, typeCoordPoint::TOP_LEFT);
-		topRightPoint.setCoordPoint(this, typeCoordPoint::TOP_RIGHT);
-		bottomRightPoint.setCoordPoint(this, typeCoordPoint::BOTTOM_RIGHT);
-		bottomLeftPoint.setCoordPoint(this, typeCoordPoint::BOTTOM_LEFT);
+    // Условное разделение текущего объекта на четыре равные части
+    topLeftPoint.setCoordPoint    (this, typeCoordPoint::TOP_LEFT);
+    topRightPoint.setCoordPoint   (this, typeCoordPoint::TOP_RIGHT);
+    bottomRightPoint.setCoordPoint(this, typeCoordPoint::BOTTOM_RIGHT);
+    bottomLeftPoint.setCoordPoint (this, typeCoordPoint::BOTTOM_LEFT);
 		
-		//
-		bool topLeftIntersect = _game->checkIntersects(topLeftPoint.coordX, topLeftPoint.coordY,
-			                                           topLeftPoint.width, topLeftPoint.height,
-			                                           this);
-		bool topRightIntersect = _game->checkIntersects(topRightPoint.coordX, topRightPoint.coordY,
-			                                            topRightPoint.width, topRightPoint.height,
-			                                            this);
-		bool bottomRightIntersect = _game->checkIntersects(bottomRightPoint.coordX, bottomRightPoint.coordY,
-			                                               bottomRightPoint.width, bottomRightPoint.height,
-			                                               this);
-		bool bottomLeftIntersect = _game->checkIntersects(bottomLeftPoint.coordX, bottomLeftPoint.coordY,
-			                                              bottomLeftPoint.width, bottomLeftPoint.height,
-			                                              this);
+    // Проверка на коллизию с другим объектом каждой четверти текущего объекта
+    /////////////////////////
+    bool topLeftIntersect = _game->checkIntersects(
+        topLeftPoint.coordX, topLeftPoint.coordY,
+        topLeftPoint.width, topLeftPoint.height, this);
 
-		bool primarySet = false;
-		float shiftSpeed = 0.6f;
+    bool topRightIntersect = _game->checkIntersects(
+        topRightPoint.coordX, topRightPoint.coordY,
+        topRightPoint.width, topRightPoint.height, this);
 
-		//
-		if (topLeftIntersect && topRightIntersect) {
-			if (getYSpeed() < shiftSpeed)
-				setYSpeed(shiftSpeed);
-			else
-				setYSpeed(getYSpeed());
-			primarySet = true;
-		}
-		if (topRightIntersect && bottomRightIntersect) {
-			if (getXSpeed() > -shiftSpeed)
-				setXSpeed(-shiftSpeed);
-			else
-				setXSpeed(getXSpeed());
-			primarySet = true;
-		}
-		if (bottomRightIntersect && bottomLeftIntersect) {
-			if (getYSpeed() > -shiftSpeed)
-				setYSpeed(-shiftSpeed);
-			else
-				setYSpeed(getYSpeed());
-			primarySet = true;
-		}
-		if (bottomLeftIntersect && topLeftIntersect) {
-			if (getXSpeed() < shiftSpeed)
-				setXSpeed(shiftSpeed);
-			else
-				setXSpeed(getXSpeed());
-			primarySet = true;
-		}
+    bool bottomRightIntersect = _game->checkIntersects(
+        bottomRightPoint.coordX, bottomRightPoint.coordY,
+        bottomRightPoint.width, bottomRightPoint.height, this);
 
-		if (primarySet)
-			return;
+    bool bottomLeftIntersect = _game->checkIntersects(
+        bottomLeftPoint.coordX, bottomLeftPoint.coordY,
+        bottomLeftPoint.width, bottomLeftPoint.height, this);
 
-		//
-		if (topLeftIntersect) {
-			switch (getDirection()) {
-			    case Direction::UP :
-				case Direction::DOWN : {
-					if (getXSpeed() < shiftSpeed)
-						setXSpeed(shiftSpeed);
-					else
-						setXSpeed(getXSpeed());
-					break;
-				}
-				case Direction::LEFT :
-				case Direction::RIGHT : {
-					if (getYSpeed() < shiftSpeed)
-						setYSpeed(shiftSpeed);
-					else
-						setYSpeed(getYSpeed());
-					break;
-				}
-			}
-		}
-		else if (topRightIntersect) {
-			switch (getDirection()) {
-			    case Direction::UP :
-				case Direction::DOWN : {
-					if (getXSpeed() > -shiftSpeed)
-						setXSpeed(-shiftSpeed);
-					else
-						setXSpeed(getXSpeed());
-					break;
-				}
-			    case Direction::LEFT :
-				case Direction::RIGHT : {
-					if (getYSpeed() < shiftSpeed)
-						setYSpeed(shiftSpeed);
-					else
-						setYSpeed(getYSpeed());
-					break;
-				}
-			}
-		}
-		else if (bottomRightIntersect) {
-			switch (getDirection()) {
-			    case Direction::UP :
-				case Direction::DOWN : {
-					if (getXSpeed() > -shiftSpeed)
-						setXSpeed(-shiftSpeed);
-					else
-						setXSpeed(getXSpeed());
-					break;
-				}
-			    case Direction::LEFT :
-				case Direction::RIGHT : {
-					if (getYSpeed() > -shiftSpeed)
-						setYSpeed(-shiftSpeed);
-					else
-						setYSpeed(getYSpeed());
-					break;
-				}
-			}
-		}
-		else if (bottomLeftIntersect) {
-			switch (getDirection()) {
-			    case Direction::UP :
-				case Direction::DOWN : {
-					if (getXSpeed() < shiftSpeed)
-						setXSpeed(shiftSpeed);
-					else
-						setXSpeed(getXSpeed());
-					break;
-				}
-			    case Direction::LEFT :
-				case Direction::RIGHT : {
-					if (getYSpeed() > -shiftSpeed)
-						setYSpeed(-shiftSpeed);
-					else
-						setYSpeed(getYSpeed());
-					break;
-				}
-			}
-		}
+    // Две соседние четверти одновременно имеют коллизию
+    bool primarySet{false};
+    // Скорость выталкивания объекта из другого объекта
+    const float SHIFT_SPEED{0.6f};
+
+    // Первичные проверки на то, что две соседние части объекта
+    // находятся внутри других объектов
+    if (topLeftIntersect && topRightIntersect) {
+        if (getYSpeed() < SHIFT_SPEED)
+            setYSpeed(SHIFT_SPEED);
+        else
+            setYSpeed(getYSpeed());
+        primarySet = true;
+    }
+    if (topRightIntersect && bottomRightIntersect) {
+        if (getXSpeed() > -SHIFT_SPEED)
+            setXSpeed(-SHIFT_SPEED);
+        else
+            setXSpeed(getXSpeed());
+        primarySet = true;
+    }
+    if (bottomRightIntersect && bottomLeftIntersect) {
+        if (getYSpeed() > -SHIFT_SPEED)
+            setYSpeed(-SHIFT_SPEED);
+        else
+            setYSpeed(getYSpeed());
+        primarySet = true;
+    }
+    if (bottomLeftIntersect && topLeftIntersect) {
+        if (getXSpeed() < SHIFT_SPEED)
+            setXSpeed(SHIFT_SPEED);
+        else
+            setXSpeed(getXSpeed());
+        primarySet = true;
+    }
+    if (primarySet)
+        return;
+
+    // Если только одна четверть имеет коллизию
+    if (topLeftIntersect)
+        switch (getDirection()) {
+            case Direction::UP :
+            case Direction::RIGHT : {
+                if (getXSpeed() < SHIFT_SPEED)
+                    setXSpeed(SHIFT_SPEED);
+                else
+                    setXSpeed(getXSpeed());
+                break;
+            }
+            case Direction::DOWN :
+            case Direction::LEFT : {
+                if (getYSpeed() < SHIFT_SPEED)
+                    setYSpeed(SHIFT_SPEED);
+                else
+                    setYSpeed(getYSpeed());
+                break;
+            }
+        }
+    else if (topRightIntersect)
+        switch (getDirection()) {
+            case Direction::UP :
+            case Direction::LEFT : {
+                if (getXSpeed() > -SHIFT_SPEED)
+                    setXSpeed(-SHIFT_SPEED);
+                else
+                    setXSpeed(getXSpeed());
+                break;
+            }
+            case Direction::DOWN :
+            case Direction::RIGHT : {
+                if (getYSpeed() < SHIFT_SPEED)
+                    setYSpeed(SHIFT_SPEED);
+                else
+                    setYSpeed(getYSpeed());
+                break;
+            }
+        }
+    else if (bottomRightIntersect)
+        switch (getDirection()) {
+            case Direction::LEFT :
+            case Direction::DOWN : {
+                if (getXSpeed() > -SHIFT_SPEED)
+                    setXSpeed(-SHIFT_SPEED);
+                else
+                    setXSpeed(getXSpeed());
+                break;
+            }
+            case Direction::UP :
+            case Direction::RIGHT : {
+                if (getYSpeed() > -SHIFT_SPEED)
+                    setYSpeed(-SHIFT_SPEED);
+                else
+                    setYSpeed(getYSpeed());
+                break;
+            }
+        }
+    else if (bottomLeftIntersect)
+        switch (getDirection()) {
+            case Direction::RIGHT :
+            case Direction::DOWN : {
+                if (getXSpeed() < SHIFT_SPEED)
+                    setXSpeed(SHIFT_SPEED);
+                else
+                    setXSpeed(getXSpeed());
+                break;
+            }
+            case Direction::LEFT :
+            case Direction::UP : {
+                if (getYSpeed() > -SHIFT_SPEED)
+                    setYSpeed(-SHIFT_SPEED);
+                else
+                    setYSpeed(getYSpeed());
+                break;
+            }
+        }
 }
 
 void GameObject::doDamage(int damage) {
