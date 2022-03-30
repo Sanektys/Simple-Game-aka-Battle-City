c92c6cb2fb80b76676c4dd7a1c7be26d1a297d73
Sanektys <Sanektisru@gmail.com>
2022-03-30 17:58:23 +0700

    Added comments to the class "EnemySpawner"
    Also made minor changes to it

--- Battle City/EnemySpawner.cpp
+++ Battle City/EnemySpawner.cpp
@@ -1,33 +1,36 @@
 #include "EnemySpawner.h"
+#include "GameObjectType.h"
 #include "TankEnemy.h"
 #include "Utils.h"
-#include "GameObjectType.h"
-#include "Level.h"
 #include "Game.h"
 
 
 EnemySpawner::EnemySpawner() {
 	_type = GameObjectType::ENEMY_SPAWNER;
 
-	_spawnTime = ENEMY_SPAWNER_SPAWN_TIME;
-	_spawnTimer = getRandomFloat(0.0, _spawnTime);
+	_spawnTime = getRandomFloat(5.0f, ENEMY_SPAWNER_SPAWN_TIME);
 
 	setPhysical(false);
 }
 
 void EnemySpawner::update(float dt) {
-	GameObject::update(dt);
-
 	_spawnTimer += dt;
 
-	if (_spawnTimer > _spawnTime) {
+    // Когда подошло время нового спавна
+	if (_spawnTimer >= _spawnTime) {
 		_spawnTimer = 0.0f;
 
+        // Текущее количество противников на уровне
 		int enemiesOnLevel = _game->getObjectsCount(GameObjectType::TANK_ENEMY);
-		int enemiesStorageLeft = ENEMIES_PER_LEVEL - enemiesOnLevel - _game->getDiedEnemiesCount();;
+        // Оставшееся общее возможное количество противников на уровне
+		int enemiesStorageLeft = ENEMIES_PER_LEVEL - enemiesOnLevel
+                                 - _game->getDiedEnemiesCount();
 
-		if (enemiesStorageLeft > 0 && enemiesOnLevel < ENEMIES_PER_LEVEL_IN_ONE_MOMENT) {
-			GameObject* tankEnemy = _game->createObject(GameObjectType::TANK_ENEMY, getX(), getY());
+		if (enemiesStorageLeft > 0
+            && enemiesOnLevel < ENEMIES_PER_LEVEL_IN_ONE_MOMENT) {
+            // Создание объекта танка противника
+			GameObject* tankEnemy = _game->createObject(
+                GameObjectType::TANK_ENEMY, getX(), getY());
 			if (tankEnemy)
 			    tankEnemy->setTextureRect(BASIC_ENEMY_TANK_IMAGE);
 		}
--- Battle City/EnemySpawner.h
+++ Battle City/EnemySpawner.h
@@ -7,10 +7,23 @@ class EnemySpawner : public GameObject
 {
     public :
         EnemySpawner();
+        ~EnemySpawner() override {}
 
+        /// <summary>
+        /// Метод обновления состояния спавнера, производит отсчёт времени
+        /// до следующего спавна и создаёт объекта танка противника
+        /// </summary>
+        /// <param name="dt">- время, прошедшее за предыдущий такт
+        /// (время обработки логики и отрисовки за прошлый раз)</param>
         void update(float dt) override;
 
     private :
-        float _spawnTimer;
-        float _spawnTime;
+        EnemySpawner(const EnemySpawner&) = delete;
+        EnemySpawner operator=(const EnemySpawner&) = delete;
+
+    private :
+        // Время задержки между созданием противников у текущего спавнера
+        float _spawnTime{0.0f};
+        // Счётчик времени спавна
+        float _spawnTimer{0.0f};
 };
